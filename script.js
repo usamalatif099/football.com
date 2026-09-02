ff6425a4facb7ce768ce1c23c2de38c6c7ee47f3
usamalatif099 <usamalatif099@gmail.com>
2026-09-02 02:52:53 -0700

    update files

---
+++ script.js
@@ -0,0 +1,37 @@
+// let open = document.getElementById('open');
+// let close = document.getElementById('close');
+// let link = document.getElementById('nav-menu');
+
+// function openmenu() {
+
+//     open.style.display = 'none';
+//     close.style.display = 'block';
+//     link.style.display = 'block'
+
+
+// }
+
+// function closemenu() {
+//     link.style.display = 'none';
+//     close.style.display = 'none';
+//     open.style.display = 'block';
+// }
+
+// navbar 2  
+
+
+let open1 = document.getElementById('menu1');
+let close1 = document.getElementById('cross1');
+let link1 = document.getElementById('nav-menu');
+
+function openmenu1() {
+    open1.style.display = 'none';
+    link1.style.display = 'block';
+    close1.style.display = 'block';
+}
+function closemenu1() {
+    open1.style.display = 'block';
+    link1.style.display = 'none';
+    close1.style.display = 'none';
+}
+
